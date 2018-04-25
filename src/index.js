badf008de7488a80b546a42c6860b8d333c238b2
Damien Varron <damien.varron@ticketmaster.com>
2018-04-25 09:10:02 -0700

    feat(package): add build script

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export { default as Button } from "./components/Button";
+
+export { default as spacing } from "./theme/spacing";
+export { default as typography } from "./theme/typography";
+export { default as colors } from "./theme/colors";
+export { default as zIndex } from "./theme/zIndex";
+export { default as constants } from "./theme/constants";
