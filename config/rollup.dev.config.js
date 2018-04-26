8ec0211c809ded1a0fb9a72516457d3d0ad7be28
Damien Varron <damien.varron@ticketmaster.com>
2018-04-26 15:05:39 -0700

    feat(package): add rollup for build
    BREAKING CHANGE: add umd, cjs and es builds

---
+++ config/rollup.dev.config.js
@@ -0,0 +1,38 @@
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/index.umd.js",
+      format: "umd",
+      sourcemap: true,
+      name: "TicketmasterPrism",
+      globals: {
+        react: "React",
+        "prop-types": "PropTypes",
+        "styled-components": "styled"
+      }
+    },
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: "dist/index.es.js",
+      format: "es",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      presets: [["env", { modules: false }], "react"],
+      plugins: ["transform-object-rest-spread"],
+      babelrc: false
+    })
+  ],
+  external: ["react", "prop-types", "styled-components"]
+};
---
+++ config/rollup.prod.config.js
@@ -0,0 +1,39 @@
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import stripPropTypes from "rollup-plugin-strip-prop-types";
+import uglify from "rollup-plugin-uglify";
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/index.umd.min.js",
+      format: "umd",
+      name: "TicketmasterPrism",
+      globals: {
+        react: "React",
+        "prop-types": "PropTypes",
+        "styled-components": "styled"
+      }
+    },
+    {
+      file: "dist/index.cjs.min.js",
+      format: "cjs"
+    },
+    {
+      file: "dist/index.es.min.js",
+      format: "es"
+    }
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      presets: [["env", { modules: false }], "react"],
+      plugins: ["transform-object-rest-spread"],
+      babelrc: false
+    }),
+    stripPropTypes({ sourceMap: false }),
+    uglify()
+  ],
+  external: ["react", "prop-types", "styled-components"]
+};
