0ce8e1344f8bcf86a9e76fe28abea2fc3c61565c
Oscar Martinez <oscar.martinez@ticketmaster.com>
2018-05-30 15:25:34 -0700

    fix(ListRow): Rename folder

--- src/components/List/__tests__/index.spec.js
+++ src/components/List/__tests__/index.spec.js
@@ -1,7 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
 
-import ListRow from "../";
+import ListRow from "../Row";
 
 describe("<ListRow />", () => {
   it("renders standard eventListRow correctly", () => {
