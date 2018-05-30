cc4ec0310d1204b71a993ad9e350bc40eeb8b7b5
Oscar Martinez <oscar.martinez@ticketmaster.com>
2018-05-30 11:01:37 -0700

    fix(Container): Uses all available space in xlarge display

--- src/components/Grid/Container.js
+++ src/components/Grid/Container.js
@@ -5,6 +5,7 @@ import spacing from "../../theme/spacing";
 const Container = styled.div`
   margin: 0 auto;
   max-width: 1440px;
+  width: 100%;
   padding-left: ${spacing.gutters.small}px;
   padding-right: ${spacing.gutters.small}px;
   box-sizing: border-box;
