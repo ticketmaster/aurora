b308c59b37655b5f79e79a559704b68c8c9a6be2
Michael Lampe <michael.lampe@ticketmaster.com>
2018-06-01 12:10:40 -0700

    feat(ImageHeader): Add BreadcrumbList and RatingBadge components to ImageHeader catalog

--- catalog/pages/header/index.js
+++ catalog/pages/header/index.js
@@ -4,11 +4,37 @@ import Header from "../../../src/components/Header";
 import Heading from "../../../src/components/Header/Heading";
 import HeaderWithImage from "../../../src/components/Header/WithImage";
 import ImageCard from "../../../src/components/ImageCard";
-import Column from "../../../src/components/Grid/Column";
+import { Column, Row } from "../../../src/components/Grid";
+import { RatingBadge } from "../../../src/components/Button";
+import { StarIcon } from "../../../src/components/Icons";
+import {
+  BreadcrumbList,
+  BreadcrumbItem
+} from "../../../src/components/Pagination";
+import colors from "../../../src/theme/colors";
+
+const breadcrumbListStyles = { color: colors.white.base };
+const starIconStyles = { marginRight: "2px" };
+const ratingBadgeStyles = { marginLeft: "12px" };
 
 export default {
   path: "/header",
   title: "Header & Heading",
-  imports: { Header, Heading, HeaderWithImage, Column, ImageCard },
+  imports: {
+    Header,
+    Heading,
+    HeaderWithImage,
+    Column,
+    Row,
+    ImageCard,
+    RatingBadge,
+    StarIcon,
+    BreadcrumbList,
+    BreadcrumbItem,
+    colors,
+    breadcrumbListStyles,
+    starIconStyles,
+    ratingBadgeStyles
+  },
   content: pageLoader(() => import("./index.md"))
 };
