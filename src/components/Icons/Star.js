6cc6d490b1086aaa9d4d9a1153409ac1443ebd52
Michael Lampe <michael.lampe@ticketmaster.com>
2018-06-01 12:08:24 -0700

    feat(Pagination): Create, test, catalog StarIcon; add titles to icons to promote accessibility

---
+++ src/components/Icons/Star.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { PropTypes } from "prop-types";
+
+const StarIcon = ({ size, color, ...props }) => (
+  <svg {...props} viewBox="0 0 11 10" width={size} height={size} fill={color}>
+    <title>Star Icon</title>
+    <path
+      fillRule="evenodd"
+      d="M5.5 7.7L2.267 9.95l1.14-3.77L.27 3.8l3.938-.08L5.5 0l1.293 3.72 3.938.08-3.139 2.38 1.14 3.77z"
+    />
+  </svg>
+);
+
+StarIcon.displayName = "StarIcon";
+
+StarIcon.propTypes = {
+  size: PropTypes.number.isRequired,
+  color: PropTypes.string.isRequired
+};
+
+export default StarIcon;
