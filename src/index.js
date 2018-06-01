e27958f4659791d3f0d3ec6993be5582b65487e9
Michael Lampe <michael.lampe@ticketmaster.com>
2018-06-01 12:11:41 -0700

    feat(Catalog): Add pagination page to catalog; export Badge, RatingBadge, BreadcrumbList, BreadcrumbItem; StarIcon from lib

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-export { default as Button } from "./components/Button";
+export { default as Button, Badge, RatingBadge } from "./components/Button";
 export { default as Card } from "./components/Card";
-export { default as ChevronIcon } from "./components/Icons/Chevron";
 export { default as Column } from "./components/Grid/Column";
 export { default as Container } from "./components/Grid/Container";
 export { default as Header } from "./components/Header";
@@ -10,13 +9,18 @@ export { default as ImageCard } from "./components/ImageCard";
 export { default as ListContainer } from "./components/List/Container";
 export { default as ListRow } from "./components/List/Row";
 export { default as NavBar } from "./components/NavBar";
-export { default as OverflowIcon } from "./components/Icons/Overflow";
 export { default as Row } from "./components/Grid/Row";
-export { default as SearchIcon } from "./components/Icons/Search";
 export { default as SearchInput } from "./components/Input/Search";
 export { default as SectionHeading } from "./components/Text/SectionHeading";
 export { default as Spacing } from "./components/Spacing";
-export { default as UserIcon } from "./components/Icons/User";
+export { BreadcrumbList, BreadcrumbItem } from "./components/Pagination";
+export {
+  ChevronIcon,
+  OverflowIcon,
+  SearchIcon,
+  UserIcon,
+  StarIcon
+} from "./components/Icons";
 
 export { default as colors } from "./theme/colors";
 export { default as constants } from "./theme/constants";
