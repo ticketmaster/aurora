be4de0ccee96772688dad0a740cd11057c433790
mlampedx <michael.lampe@ticketmaster.com>
2018-06-01 17:54:35 -0700

    Rename Button filenames to Base

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,5 @@
-export { default as Button } from "./Button";
-export { StyledButton, StyledButtonLink } from "./Button.styles";
+export { default as Button } from "./Base";
+export { StyledButton, StyledButtonLink } from "./Base.styles";
 export { default as Badge } from "./Badge";
 export { default as StyledBadge } from "./Badge.styles";
 export { default as RatingBadge } from "./RatingBadge";
