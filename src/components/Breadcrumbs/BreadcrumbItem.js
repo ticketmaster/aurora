d649f07d1cbb9f1fe0ab0ea2e3677cc7b536b731
mlampedx <michael.lampe@ticketmaster.com>
2018-06-29 17:08:02 -0700

    feat(Breadcrumbs) Truncate final Breadcrumb.Item

--- src/components/Breadcrumbs/BreadcrumbItem.js
+++ src/components/Breadcrumbs/BreadcrumbItem.js
@@ -1,27 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import styled from "styled-components";
 import typography from "../../theme/typography";
 import { Link, StyledText } from "../Text";
 
+const InlineLi = styled.li`
+  white-space: nowrap;
+`;
+
 const Text = StyledText.withComponent("span");
 
-const BreadcrumbItem = ({ position, children, ...props }) => {
-  const { href } = props;
+const BreadcrumbItem = ({ position, children, href, ...props }) => {
   const Tag = href ? Link : Text;
 
   return (
-    <li
+    <InlineLi
       itemProp="itemListElement"
       itemScope
       itemType="http://schema.org/ListItem"
     >
-      <Tag itemProp="item" {...props} fontSize={typography.size.uno}>
+      <Tag
+        itemProp="item"
+        fontSize={typography.size.uno}
+        href={href || null}
+        {...props}
+      >
         {children}
       </Tag>
       {children && <meta itemProp="name" content={children} />}
       {position && <meta itemProp="position" content={position} />}
-    </li>
+    </InlineLi>
   );
 };
 
