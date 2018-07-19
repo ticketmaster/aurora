e9db7ad714049054b0fa8716a4ce7a61ec670689
monica.varhale <monica.varhale@ticketmaster.com>
2018-07-19 16:55:42 -0700

    Update ListContainer failing tests

--- src/components/List/__tests__/Container.spec.js
+++ src/components/List/__tests__/Container.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, Simulate } from "react-testing-library";
+import { render, Simulate, renderIntoDocument } from "react-testing-library";
 
 import {
   listItems,
@@ -11,10 +11,19 @@ import { ListContainer, ListRow, Section, SectionItem } from "../";
 const onOverflowButtonClick = ({ scope, index }) => ({ event }) => ev => {}; // eslint-disable-line
 
 describe("<ListContainer />", () => {
-  const modalRoot = global.document.createElement("div");
-  modalRoot.setAttribute("id", "modal-root");
-  const body = global.document.querySelector("body");
-  body.appendChild(modalRoot);
+  beforeEach(() => {
+    const modalRoot = global.document.createElement("div");
+    modalRoot.setAttribute("id", "modal-root");
+    modalRoot.setAttribute("data-testid", "modal-root");
+    const body = global.document.querySelector("body");
+    body.appendChild(modalRoot);
+  });
+
+  afterEach(() => {
+    const body = global.document.querySelector("body");
+    const modalRoot = global.document.getElementById("modal-root");
+    body.removeChild(modalRoot);
+  });
 
   it("renders ListContainer correctly without any expanded sections", () => {
     const { container } = render(
@@ -61,7 +70,8 @@ describe("<ListContainer />", () => {
             ))}
           </ListRow>
         ))}
-      </ListContainer>
+      </ListContainer>,
+      global.document.querySelector("body")
     );
     Simulate.click(container.querySelector(".button--expand-or-collapse"));
     expect(container.firstChild).toMatchSnapshot();
@@ -100,7 +110,7 @@ describe("<ListContainer />", () => {
   });
 
   it("opens the bottomSheet for the row when clicked on overflow button", () => {
-    const { container } = render(
+    const { container } = renderIntoDocument(
       <ListContainer>
         {listItems.map((item, index) => (
           <ListRow
@@ -127,43 +137,11 @@ describe("<ListContainer />", () => {
       </ListContainer>
     );
     Simulate.click(container.querySelector(".button--more-info"));
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it("closes the bottomSheet for the row when clicked on overlay", () => {
-    const { container } = render(
-      <ListContainer onRowCollapse={() => {}}>
-        {listItems.map((item, index) => (
-          <ListRow
-            key={item.rowId}
-            rowItem={item}
-            index={index}
-            onOverflowClick={() => {}}
-          >
-            {sections.map(section => (
-              <Section
-                title={section.title}
-                totalSections={4}
-                key={section.title}
-              >
-                {section.items
-                  .slice(0, 4)
-                  .map(sectionItem => (
-                    <SectionItem item={sectionItem} key={sectionItem.title} />
-                  ))}
-              </Section>
-            ))}
-          </ListRow>
-        ))}
-      </ListContainer>
-    );
-    Simulate.click(container.querySelector(".button--more-info"));
-    // Simulate.click(container.querySelector(".container--overlay"));
-    expect(container.firstChild).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 
   it("closes the bottomSheet for the row when clicked on cross icon", () => {
-    const { container } = render(
+    const { container, queryByTestId } = renderIntoDocument(
       <ListContainer onRowCollapse={() => {}}>
         {listItems.map((item, index) => (
           <ListRow
@@ -190,7 +168,8 @@ describe("<ListContainer />", () => {
       </ListContainer>
     );
     Simulate.click(container.querySelector(".button--more-info"));
-    // Simulate.click(container.querySelector(".button--cancel"));
-    expect(container.firstChild).toMatchSnapshot();
+    const portal = queryByTestId("modal-root");
+    Simulate.click(portal.querySelector(".button--cancel"));
+    expect(container).toMatchSnapshot();
   });
 });
