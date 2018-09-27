352e3e29ed229d52deac2ef4d899d1800e8df7d8
Nikolay Yonchev <nikolay.yonchev@gmail.com>
2018-09-28 00:04:35 +0300

    feat(LoaderRing): infinite loader component (#205)
    * feat(LoaderRing): add loader ring component
    
    * feat(LoaderRing): infinite loader component

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ export {
 export { default as Card } from "./components/Card";
 export { default as Column } from "./components/Grid/Column";
 export { default as Container } from "./components/Grid/Container";
+export { default as LoaderRing } from "./components/LoaderRing";
 export { default as DrawerProvider } from "./components/Drawer/Provider";
 export { default as Gradient } from "./components/Gradient";
 export { default as Header } from "./components/Header";
