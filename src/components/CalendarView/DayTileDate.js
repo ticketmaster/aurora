dad9a2d6297548056d5bc373ed605f8a73e5ed09
shYkiSto <shykisto@gmail.com>
2018-10-03 12:47:54 -0700

    fix(CalendarView): update DayTile color scheme and MoreButton styles (#229)
    * fix(CalendarView): update DayTile color scheme and MoreButton styles
    
    * chore(aurora): add yarn.lock file

--- src/components/CalendarView/DayTileDate.js
+++ src/components/CalendarView/DayTileDate.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import classnames from "classnames";
 
 import { Text } from "../Text";
 
-import { colors } from "../../theme";
+import { getThemeValue } from "../../utils";
 
 const DayTileDateContainer = styled.div`
   display: flex;
@@ -12,29 +13,28 @@ const DayTileDateContainer = styled.div`
   height: 30px;
   justify-content: center;
   align-items: center;
-  background-color: ${colors.white.base};
+  background-color: ${getThemeValue("white", "base")};
+`;
+const DayTileDateText = styled(Text).attrs({
+  size: "kilo",
+  weight: "semiBold",
+  className: ({ accent }) =>
+    classnames("day-tile-date__text", {
+      "day-tile-date__text--accent": accent
+    })
+})`
+  color: ${getThemeValue("gray02")};
+
+  &.day-tile-date__text--accent {
+    color: ${getThemeValue("accent01", "dark")};
+  }
 `;
 
-const ACCENT_TEXT_PROPS = {
-  variant: "accent",
-  accent: "heliotrope"
-};
-
-const DEFAULT_TEXT_PROPS = {
-  disabled: true
-};
-
-const DayTileDate = ({ children, accent }) => {
-  const textProps = accent ? ACCENT_TEXT_PROPS : DEFAULT_TEXT_PROPS;
-
-  return (
-    <DayTileDateContainer>
-      <Text size="kilo" weight="semiBold" {...textProps}>
-        {children}
-      </Text>
-    </DayTileDateContainer>
-  );
-};
+const DayTileDate = ({ children, accent }) => (
+  <DayTileDateContainer>
+    <DayTileDateText accent={accent}>{children}</DayTileDateText>
+  </DayTileDateContainer>
+);
 
 DayTileDate.propTypes = {
   children: PropTypes.node.isRequired,
