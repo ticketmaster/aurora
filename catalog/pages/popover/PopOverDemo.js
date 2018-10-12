d2f737beae5bc2730fbe3d2dc08acfb2fcf8042e
Nikolay Yonchev <nikolay_yonchev@epam.com>
2018-10-12 19:07:30 +0300

    fix(PopOverCatalog): add popover example to catalog

---
+++ catalog/pages/popover/PopOverDemo.js
@@ -0,0 +1,96 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import PopOver from "../../../src/components/PopOver";
+
+const Container = styled.div`
+  width: 95%;
+  height: 400px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+`;
+
+const PopOverButton = styled.div`
+  width: 100px;
+  height: 50px;
+  border: 1px solid red;
+  display: inline-block;
+`;
+
+class PopOverDemo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showPopOver: false,
+      mouseX: 0,
+      mouseY: 0
+    };
+
+    this.containerRef = React.createRef();
+  }
+
+  buttonSelect = e => {
+    const { withinContainer } = this.props;
+    const data = withinContainer
+      ? PopOver.getDimensionsFromEvent(e, this.containerRef.current)
+      : PopOver.getDimensionsFromEvent(e);
+    const { showPopOver } = this.state;
+
+    this.setState({
+      showPopOver: !showPopOver,
+      ...data
+    });
+  };
+
+  render() {
+    const { showPopOver, ...position } = this.state;
+    return (
+      <Container innerRef={this.containerRef}>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between"
+          }}
+        >
+          <PopOverButton onClick={this.buttonSelect} />
+          <PopOverButton onClick={this.buttonSelect} />
+        </div>
+        <div style={{ width: "100%", textAlign: "center" }}>
+          <PopOverButton onClick={this.buttonSelect} />
+        </div>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between"
+          }}
+        >
+          <PopOverButton onClick={this.buttonSelect} />
+          <PopOverButton onClick={this.buttonSelect} />
+        </div>
+        <PopOver
+          isVisible={showPopOver}
+          position={{ ...position }}
+          reduceTop={76}
+        >
+          Some text to be rendered in the pop over component. Some text to be
+          rendered in the pop over component.
+        </PopOver>
+      </Container>
+    );
+  }
+}
+
+PopOverDemo.propTypes = {
+  withinContainer: PropTypes.bool
+};
+
+PopOverDemo.defaultProps = {
+  withinContainer: false
+};
+
+export default PopOverDemo;
---
+++ catalog/pages/popover/index.md
@@ -0,0 +1,19 @@
+### PopOver
+
+PopOver Component
+
+### Props
+
+```table
+span: 6
+rows:
+  - Prop: children
+    Type: node
+    Default:
+    Notes: Render as the PopOver's children
+```
+
+```react
+---
+<PopOverDemo withinContainer />
+```
