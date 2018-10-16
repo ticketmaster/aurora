da8c3e3a3a044c85380fb429fc6b29e88755dbb2
Kostya Bahinskyi <bah.kostya@gmail.com>
2018-10-16 15:28:13 -0700

    fix(List): remove react-addons-css-transition-group and refactor to use react-transition-group (#258)

--- src/components/List/Container.js
+++ src/components/List/Container.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import { CSSTransition } from "react-transition-group";
 
 import { ContainerProvider } from "./Context";
 import DisplayFor from "../DeviceSize";
@@ -42,7 +42,6 @@ class ListContainer extends Component {
     }
     if (this.state.mobilePortalContent) {
       this.setState({
-        mobilePortalContent: null,
         openIndex: ITEMS_COLLAPSED
       });
 
@@ -90,45 +89,48 @@ class ListContainer extends Component {
 
   render() {
     const { children, ...rest } = this.props;
+    const { mobilePortalContent, desktopPortalContent, openIndex } = this.state;
+    const isBottomSheetOpen =
+      openIndex !== ITEMS_COLLAPSED && !!mobilePortalContent;
+
     return (
       <Container onClick={this.onExpandOrCollapse} {...rest}>
         <ContainerProvider value={this.state}>
           {children}
-
           <DisplayFor small>
             <Portal>
-              <ReactCSSTransitionGroup
-                transitionName="bottom-sheet"
-                component={this.state.mobilePortalContent ? Backdrop : "span"}
-                transitionAppear
-                transitionAppearTimeout={300}
-                transitionEnterTimeout={300}
-                transitionLeaveTimeout={300}
+              {isBottomSheetOpen && <Backdrop />}
+              <CSSTransition
+                classNames="bottom-sheet"
+                timeout={300}
+                in={isBottomSheetOpen}
+                appear
+                unmountOnExit
+                mountOnEnter
               >
-                {this.state.openIndex !== ITEMS_COLLAPSED &&
-                  this.state.mobilePortalContent && (
-                    <BottomSheet index={this.state.openIndex}>
-                      {this.state.mobilePortalContent}
-                    </BottomSheet>
-                  )}
-              </ReactCSSTransitionGroup>
+                <BottomSheet index={openIndex}>
+                  {mobilePortalContent}
+                </BottomSheet>
+              </CSSTransition>
             </Portal>
           </DisplayFor>
 
           <DisplayFor medium large xLarge>
             <Portal>
-              <ReactCSSTransitionGroup
-                transitionName="modal"
-                component={this.state.desktopPortalContent ? Backdrop : "span"}
-                transitionAppear
-                transitionAppearTimeout={300}
-                transitionEnterTimeout={300}
-                transitionLeaveTimeout={100}
+              {desktopPortalContent && <Backdrop />}
+              <CSSTransition
+                classNames="modal"
+                timeout={{
+                  enter: 300,
+                  exit: 100
+                }}
+                in={!!desktopPortalContent}
+                appear
+                unmountOnExit
+                mountOnEnter
               >
-                {this.state.desktopPortalContent && (
-                  <Modal>{this.state.desktopPortalContent}</Modal>
-                )}
-              </ReactCSSTransitionGroup>
+                <Modal>{desktopPortalContent}</Modal>
+              </CSSTransition>
             </Portal>
           </DisplayFor>
         </ContainerProvider>
