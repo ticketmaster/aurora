03443ff055da8a1b092c7fcf357a299fe2fd5557
Michael <mlampesd@gmail.com>
2018-10-31 12:00:43 -0700

    feat(ConnProvider): Create DeviceConnectionProvider, DeviceConnectionConsumer; DisplayOn components (#272)
    * feat(ConnProvider): Create DeviceConnectionProvider, DeviceConnectionConsumer; DisplayOn components
    
    * feat(ConnProvider): Ignore onConnectionChange method from coverage

---
+++ src/components/DeviceConnection/index.js
@@ -0,0 +1,3 @@
+export { default as DeviceConnectionProvider } from "./Provider";
+export { Consumer as DeviceConnectionConsumer } from "./Context";
+export { default as DisplayOn } from "./DisplayOn";
