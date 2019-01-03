9d36dd97bef817aa3e44ac644934723af1b280c7
Michael <mlampesd@gmail.com>
2019-01-02 19:52:33 -0800

    feat(Icons): Add color prop to Apple, Instagram, and LinkedIn icons (#369)

--- src/components/Icons/__tests__/Instagram.spec.js
+++ src/components/Icons/__tests__/Instagram.spec.js
@@ -2,13 +2,24 @@ import React from "react";
 import renderer from "react-test-renderer";
 
 import InstagramIcon from "../Instagram";
+import themes from "../../../theme/colorThemes";
+
+const { tm } = themes;
 
 describe("InstagramIcon", () => {
   const children = <title>Instagram Icon</title>;
 
   it("renders correctly", () => {
     const component = renderer.create(
-      <InstagramIcon size={22} color="#000">
+      <InstagramIcon size={22}>{children}</InstagramIcon>
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it("renders correctly when a color prop is passed", () => {
+    const component = renderer.create(
+      <InstagramIcon color={tm.primary.base} size={22}>
         {children}
       </InstagramIcon>
     );
