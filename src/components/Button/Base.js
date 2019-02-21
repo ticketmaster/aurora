8fa1542c9715526740762b9bfb47329be1d12e01
Stanislav Chernetsky <Stanislav_Chernetskyi@epam.com>
2019-02-21 16:43:26 +0200

    fix(Button): avoid focus on click

--- src/components/Button/Base.js
+++ src/components/Button/Base.js
@@ -1,36 +1,74 @@
-import React from "react";
+/* global window: true */
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import { StyledButton, StyledButtonLink } from "./Base.styles";
 import { SIZES, REGULAR, BUTTON_VARIANTS, STANDARD } from "../constants";
 import { getRelByTarget } from "../../utils/link";
 
-const Button = ({ variant, size, children, ...rest }) => {
-  const { href } = rest;
-
-  if (href) {
-    const { rel, target } = rest;
-    const validatedRel = getRelByTarget(target, rel);
-
-    return (
-      <StyledButtonLink
-        variant={variant}
-        size={size}
-        rel={validatedRel}
-        as="a"
-        {...rest}
-      >
-        {children}
-      </StyledButtonLink>
-    );
+class Button extends Component {
+  componentDidMount() {
+    if (!this.props.href) {
+      // this functionality is required to avoid focus outline on click but keep it on tab focus
+      this.button.current.addEventListener("focus", this.focusHandler);
+      this.button.current.addEventListener("blur", this.blurHandler);
+    }
   }
 
-  return (
-    <StyledButton variant={variant} size={size} {...rest}>
-      {children}
-    </StyledButton>
-  );
-};
+  componentWillUnmount() {
+    // in case user leaves a page before onBlur is triggered
+    window.removeEventListener("keyup", this.activateFocusStyles);
+  }
+
+  focusHandler = () => {
+    window.addEventListener("keyup", this.activateFocusStyles);
+  };
+
+  blurHandler = () => {
+    this.button.current.classList.add("noFocus");
+    window.removeEventListener("keyup", this.activateFocusStyles);
+  };
+
+  activateFocusStyles = () => {
+    this.button.current.classList.remove("noFocus");
+  };
+
+  button = React.createRef();
+
+  render() {
+    const { variant, size, children, ...rest } = this.props;
+    const { href } = rest;
+
+    if (href) {
+      const { rel, target } = rest;
+      const validatedRel = getRelByTarget(target, rel);
+
+      return (
+        <StyledButtonLink
+          variant={variant}
+          size={size}
+          rel={validatedRel}
+          as="a"
+          {...rest}
+        >
+          {children}
+        </StyledButtonLink>
+      );
+    }
+
+    return (
+      <StyledButton
+        variant={variant}
+        size={size}
+        {...rest}
+        className={`${rest.className || ""} noFocus`}
+        ref={this.button}
+      >
+        {children}
+      </StyledButton>
+    );
+  }
+}
 
 Button.propTypes = {
   variant: PropTypes.oneOf(BUTTON_VARIANTS),
