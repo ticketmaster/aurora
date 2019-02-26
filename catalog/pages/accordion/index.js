f5b5719f8d98736d6649859cfb2cadcb8d51ceef
Rob.Rosario <robbysrosario@gmail.com>
2019-02-26 15:33:12 -0800

    update: added default, styled and nested accordion examples

--- catalog/pages/accordion/index.js
+++ catalog/pages/accordion/index.js
@@ -1,9 +1,10 @@
 import { pageLoader } from "catalog";
 import Accordion from "../../../src/components/Accordion";
+import {DATA} from "./mock";
 
 export default {
   path: "/accordion",
   title: "Accordion",
-  imports: {Accordion},
+  imports: {Accordion, DATA},
   content: pageLoader(() => import("./index.md"))
 };
