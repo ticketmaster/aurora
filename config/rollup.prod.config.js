02fd59e74502bc6624f13ab5248f30fe8165a7fc
Stanislav Chernetsky <Stanislav_Chernetskyi@epam.com>
2019-03-11 14:30:10 +0200

    fix(rollup): update rollup, change uglify to terser

--- config/rollup.prod.config.js
+++ config/rollup.prod.config.js
@@ -1,6 +1,6 @@
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
-import uglify from "rollup-plugin-uglify";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/index.js",
@@ -41,7 +41,7 @@ export default {
       ],
       babelrc: false
     }),
-    uglify()
+    terser()
   ],
   external: [
     "react",
