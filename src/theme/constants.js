640b6602e68a150e0eb1cf9e10572f4647dab99a
mlampedx <mlampesd@gmail.com>
2019-07-11 15:29:00 -0700

    feat(Breakpoints): Increase breakpoint min-widths by 1px; decrease breakpoint max-widths by 1px to match industry standards

--- src/theme/constants.js
+++ src/theme/constants.js
@@ -5,14 +5,14 @@ const constants = {
     large: "4px"
   },
   breakpoints: {
-    small: "(max-width: 767px)",
-    medium: "(min-width: 768px)",
-    large: "(min-width: 1024px)",
+    small: "(max-width: 768px)",
+    medium: "(min-width: 769px)",
+    large: "(min-width: 1025px)",
     xLarge: "(min-width: 1440px)",
-    xSmallAndDown: "(max-width: 479px)",
-    smallAndUp: "(min-width: 480px)",
-    mediumAndUp: "(min-width: 768px)",
-    largeAndUp: "(min-width: 1024px)",
+    xSmallAndDown: "(max-width: 480px)",
+    smallAndUp: "(min-width: 481px)",
+    mediumAndUp: "(min-width: 769px)",
+    largeAndUp: "(min-width: 1025px)",
     xLargeAndUp: "(min-width: 1440px)"
   },
   easing: {
