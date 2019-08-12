706483144c7db234c1f375efca5c0593cc11bba6
Siarhei_Fedarovich <shykisto@gmail.com>
2019-08-12 12:20:18 -0700

    test(aurora): update jest-styled-components to 6.3.3 and update snapshots

--- src/components/RangeSlider/__tests__/SliderParticles.spec.js
+++ src/components/RangeSlider/__tests__/SliderParticles.spec.js
@@ -1,6 +1,5 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import "jest-styled-components";
 import Handle from "../Handle";
 import Track from "../Track";
 
--- src/components/SearchInput/__tests__/SearchInput.spec.js
+++ src/components/SearchInput/__tests__/SearchInput.spec.js
@@ -1,6 +1,5 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import "jest-styled-components";
 import SearchInput from "../index";
 import { Cancel, SearchSuggest } from "../Search.styles";
 import { getSearchHeight } from "../constants";
