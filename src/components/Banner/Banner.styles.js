0987ef815f0a0d0e2d792a30175d3ee889d9b2b0
Francis Fortier <francis.fortier@ticketmaster.com>
2020-01-06 09:26:46 -0500

    fix(Banner): Overrides on the close button were not applied correctly if the DOM tree only contains a banner

--- src/components/Banner/Banner.styles.js
+++ src/components/Banner/Banner.styles.js
@@ -69,25 +69,32 @@ export const Content = styled(Text)`
   }
 `;
 
-export const CloseButton = styled(Button, StyledButton)`
-  background-color: transparent;
-  border: none;
-  padding: 0;
-  position: absolute;
-  top: ${spacing.cozy};
-  right: 3px;
-  width: 42px;
-  min-width: 42px;
-  height: 42px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &:active,
-  &:hover {
+/* stylelint-disable */
+// Stylelint prints the following error: Unexpected duplicate selector ".selector35" on this line ${StyledButton}&, but it is required to do it
+// this way because once the button is rendered, the styles are flatten and all set to the button element. So the StyledButton styles and the
+// CloseButton overrides must be set at the same level.
+export const CloseButton = styled(Button)`
+  ${StyledButton}& {
     background-color: transparent;
+    border: none;
+    padding: 0;
+    position: absolute;
+    top: ${spacing.cozy};
+    right: 3px;
+    width: 42px;
+    min-width: 42px;
+    height: 42px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:active,
+    &:hover {
+      background-color: transparent;
+    }
   }
 `;
+/* stylelint-enable */
 
 export const CloseIcon = styled(CrossIcon).attrs({
   color: "currentColor"
